4ddb82d38eb1d3d49b73424416c6c0b922324540
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-09-03 15:55:47 +0300

    Updates

---
+++ precache-manifest.d2709398213a22a03aac326bca521bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9135036775bc7d365388bf9d840fe266",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "5e2d78e573e5d37c129b",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.492b5c1c.chunk.css"
+  },
+  {
+    "revision": "b40ebe305fff4bc64660",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.ae7719af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.ae7719af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2d78e573e5d37c129b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.08d291e6.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
